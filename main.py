c81c97cf279a091bb5c91e0f77e1cc868102c798
Thomas Garbr <artyom.lunkov@gmail.com>
2023-05-07 17:19:31 +0300

    Added get requests for database slicing

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 templates = Jinja2Templates(directory="")
 
@@ -22,15 +23,37 @@ def get_db():
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/schools/", response_class=HTMLResponse)
-async def show_schools(request:Request):
-    db = SessionLocal()
+async def show_schools(request:Request,db: Session = Depends(get_db)):
     amount, schools = len(db.query(models.School).all()), db.query(models.School).all()
     return templates.TemplateResponse("index.html", {"request": request, "amount": str(amount), "schools": schools})
 
-@app.get("/schools/{school_id}")
-def read_user(school_id: int, db: Session = Depends(get_db)):
-    user = db.query(models.School).filter(models.School.id == school_id).first()
-    if user is None:
-        raise HTTPException(status_code=404, detail="School is ehh")
-    return user
+
+@app.get("/school/", response_class=HTMLResponse)
+async def show_school(school_id: int, request:Request, db: Session = Depends(get_db)):
+    school = db.query(models.School).filter(models.School.id == school_id).first()
+    if school_id <= 0:
+        raise HTTPException(status_code=404, detail="Id must be positive number! ")
+    if school is None:
+        raise HTTPException(status_code=404, detail="No such id for school")
+    return templates.TemplateResponse("index.html", {"request": request, "schools": [school]})
+
+
+@app.get("/list/", response_class=HTMLResponse)
+async def show_list(start: int, end: int, request:Request, db: Session = Depends(get_db)):
+    if start <= 0:
+        raise HTTPException(status_code=404, detail="Start must be positive number!")
+    if end > len(db.query(models.School).all()):
+        raise HTTPException(status_code=404, detail="Out of index!")
+    if start > end:
+        raise HTTPException(status_code=404, detail="Start after end!")
+    schools = db.query(models.School).all()
+    return templates.TemplateResponse("index.html", {"request": request, "schools": schools[start-1:end]})
+
+
+@app.get("/count/")
+def show_amount():
+    db = SessionLocal()
+    amount = len(db.query(models.School).all())
+    return amount
