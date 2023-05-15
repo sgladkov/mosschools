c1b25c87e44d1e1c32d29dd131f912806463629c
Thomas Garbr <artyom.lunkov@gmail.com>
2023-05-15 16:34:04 +0300

    minor improves

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from database import SessionLocal, engine
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
-
+from fastapi.staticfiles import StaticFiles
+from pyproj import Proj, transform
+from typing import List
+from geopy.distance import geodesic
+from fastapi.responses import JSONResponse
+import re
 app = FastAPI()
-templates = Jinja2Templates(directory="")
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 models.Base.metadata.create_all(bind=engine)
+
+
+
+
 
 def get_db():
     db = SessionLocal()
@@ -30,6 +39,15 @@ async def show_schools(request:Request,db: Session = Depends(get_db)):
     return templates.TemplateResponse("index.html", {"request": request, "amount": str(amount), "schools": schools})
 
 
+@app.get("/schools_adm/", response_class=HTMLResponse, name='adm')
+async def show_areas(area: str, request:Request, db: Session = Depends(get_db)): 
+    schools = db.query(models.School).filter(models.School.admarea == area).all()
+    data = {}
+    for i, school in enumerate(schools):
+        data[f's{i}'] = [school.id, school.name, school.orgtype, school.admarea, school.district, school.address, school.site]
+    return JSONResponse(content=data)
+
+
 @app.get("/school/", response_class=HTMLResponse)
 async def show_school(school_id: int, request:Request, db: Session = Depends(get_db)):
     school = db.query(models.School).filter(models.School.id == school_id).first()
@@ -38,6 +56,18 @@ async def show_school(school_id: int, request:Request, db: Session = Depends(get
     if school is None:
         raise HTTPException(status_code=404, detail="No such id for school")
     return templates.TemplateResponse("index.html", {"request": request, "schools": [school]})
+
+@app.get("/schoolcoords/")
+async def take_coords(school_id: int, request:Request, db: Session = Depends(get_db)):
+    school = db.query(models.School).filter(models.School.id == school_id).first()
+    pattern = r'\d+'
+    numbers = re.findall(pattern, school.coords)
+    lt, lg = f'{numbers[0]}.{numbers[1]}', f'{numbers[2]}.{numbers[3]}'
+    if school_id <= 0:
+        raise HTTPException(status_code=404, detail="Id must be positive number! ")
+    if school is None:
+        raise HTTPException(status_code=404, detail="No such id for school")
+    return float(lt), float(lg), JSONResponse(content={'name': school.name, 'fullname': school.fullname, 'admarea':school.admarea, 'district': school.district, 'address':school.address, 'site': school.site, 'chiefpos': school.chiefpos, 'chief': school.chief, 'phone': school.phone})
 
 
 @app.get("/list/", response_class=HTMLResponse)
@@ -57,3 +87,22 @@ def show_amount():
     db = SessionLocal()
     amount = len(db.query(models.School).all())
     return amount
+
+
+@app.get("/schools-in-radius")
+def get_schools_in_radius(lat: float, lng: float, radius: int, db: Session = Depends(get_db)):
+    schools_in_radius = []
+    pattern = r'\d+'
+    for school in db.query(models.School).all():
+        numbers = re.findall(pattern, school.coords)
+        lt, lg = f'{numbers[0]}.{numbers[1]}', f'{numbers[2]}.{numbers[3]}'
+        distance_to_school = distance(lat, lng, lt, lg)
+        if distance_to_school <= radius:
+            schools_in_radius.append(school.id)
+    return schools_in_radius
+
+def distance(lat1, lng1, lat2, lng2):
+    
+    coord1 = (lat1, lng1)
+    coord2 = (lat2, lng2)
+    return geodesic(coord1, coord2).meters
